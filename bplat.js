38cdb8b7ec78420c622d4e0d7a4d5cf235d2d362
devprashant <devprashantdawar@gmail.com>
2017-06-03 21:04:10 +0530

    little changes

--- bplat.js
+++ bplat.js
@@ -3,6 +3,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var appRouter = require('./routes');
 var mongoose = require('mongoose');
+var logger = require('morgan');
 
 mongoose.connect('mongodb://localhost:27017/bplat');
 
@@ -10,6 +11,7 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
